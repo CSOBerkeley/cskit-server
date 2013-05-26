0d1fbd622b37318e9087609c8bf1a49622a66eb9
Cameron Dutro <camertron@gmail.com>
2013-05-25 20:17:29 -0700

    Add jsonp support

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source "https://rubygems.org"
 
 gem "puma", "~> 2.0.1"
 gem "json", "~> 1.8.0"
+gem "rack-contrib", "~> 1.1.0"
 gem "grape", :github => "camertron/grape"
 
 gem "cskit", :github => "camertron/cskit-rb"
--- lib/cskit-server/api/v1/base.rb
+++ lib/cskit-server/api/v1/base.rb
@@ -1,13 +1,18 @@
 # encoding: UTF-8
 
+require 'rack/contrib'
+
 module CSKit
   module Api
     module V1
 
       class Base < Grape::API
 
+        use Rack::JSONP
+
         version 'v1', :using => :path
         # rescue_from :all
+        default_format :json
 
         mount CSKit::Api::V1::LessonEndpoints => "/"
 
